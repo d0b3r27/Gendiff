e7715be1d5538d2de06c88320151fedcb0d8fa58
Shishkin Andrey <shish27@yandex.ru>
2024-06-16 16:03:02 +0500

    add output.js file

---
+++ src/output.js
@@ -0,0 +1,15 @@
+const output = (tree) => {
+  const result = tree.map((node) => {
+    if (node.type === 'deleted') {
+      return `- ${node.key}: ${node.value} \n`
+    } else if (node.type === 'added') {
+      return `+ ${node.key}: ${node.value} \n`
+    } else if (node.type === 'changed') {
+      return `+ ${node.key}: ${node.value1} \n- ${node.key}: ${node.value2}\n`
+    } return `  ${node.key}: ${node.value} \n`
+
+  }).join('');
+  return `{\n${result}}`;
+};
+
+export default output;
